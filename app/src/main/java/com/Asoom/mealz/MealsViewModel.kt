18d067e318fe009465af36a381dec01802af8a6c
Asmaa Khaled Gomaa <asmaa.kh.gomaa@gmail.com>
2023-08-14 18:30:11 +0300

    MealsViewModel

--- app/src/main/java/com/Asoom/mealz/MealsViewModel.kt
+++ app/src/main/java/com/Asoom/mealz/MealsViewModel.kt
@@ -3,18 +3,25 @@ package com.Asoom.mealz
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.asoom.domain.entity.CategoryResponse
 import com.asoom.domain.usecase.getMealz
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
 class MealsViewModel @Inject constructor(private val getMealsUseCase: getMealz): ViewModel(){
+
+    private val  _categories: MutableStateFlow<CategoryResponse?> = MutableStateFlow(null)
+    val categories: StateFlow<CategoryResponse?> = _categories
+
      fun getMeals(){
          viewModelScope.launch {
              try {
-                 getMealsUseCase()
+                  _categories.value = getMealsUseCase()
              }catch (e:Exception){
 
                  Log.e("MealsViewModel",e.message.toString())
